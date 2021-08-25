bdfabcf61924897927b08093a2e8862eb4e89c91
daboboa <67788107+daboboa@users.noreply.github.com>
2021-08-25 17:20:24 +1000

    Instantiate prefabs through code

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
+    //define new public red object variable
     public GameObject redObj;
-    
+    //define new private blue object variable and make it available in inspector
+    [SerializeField]
+    private GameObject blueObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2,0,0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2,0,0), Quaternion.identity);
     }
 
     // Update is called once per frame
