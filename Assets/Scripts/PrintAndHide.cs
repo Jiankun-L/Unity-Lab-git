eb43c5c62fd40bbbca3bf4e0785eb8fdd5f23bf9
daboboa <67788107+daboboa@users.noreply.github.com>
2021-08-25 20:51:45 +1000

    deactivate objects and components

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,11 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     private int i = 3;
+    private int ranNumber;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //se the random number
+        ranNumber = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -18,5 +20,15 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        //deactivate the red game object if meet the condition
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        //deactivate the blue game object if meet the condition
+        if (gameObject.tag == "Blue" && i == ranNumber)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
     }
 }
